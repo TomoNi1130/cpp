e6c21d188945165cd761d170cfe3fb1c7752b361
TomoNi11 <tomoki201130@gmail.com>
2024-11-10 15:57:18 +0900

    202411102

--- otherlib/matplotlibcpp_h/matplotlibcpp_h.cpp
+++ otherlib/matplotlibcpp_h/matplotlibcpp_h.cpp
@@ -16,15 +16,23 @@ namespace plt = matplotlibcpp;
 int main()
 {
    int n = 1000;
-   std::vector<double> x(n), y(n);
+   std::vector<double> x(n), y(n); // x,yともにnこのdouble型の要素を持つように定義する(この要素たちを使って点を表現する)
 
    for (int i = 0; i < n; ++i)
    {
-      x[i] = i;
-      y[i] = sin(0.1 * i);
+      x[i] = i;            // x軸はそのまま増えていく
+      y[i] = sin(0.1 * i); // y軸のみsinカーブを描くように
    }
-
    plt::plot(x, y);
+
+   // グラフに名前を付けることもできる
+   std::vector<double> nx(n), ny(n);
+   for (int i = 0; i < n; ++i)
+   {
+      nx[i] = i;
+      ny[i] = 0.0005 * i - 0.2;
+   }
+   plt::named_plot("0.0005x-0.2", nx, ny);
    plt::show();
 
    return 0;
